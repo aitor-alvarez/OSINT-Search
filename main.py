cf8cf782a4ac223bf708602b9b0a4ac42cc5c362
aitor-arronte <aitoraalvarez@gmail.com>
2022-07-08 08:03:15 +0200

    addedd code

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from osint import search_osint_files
 
 if __name__ == 'main':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-f', '--file_type', help='File type. Default Yara.')
+	parser.add_argument('-f', '--file_type', help='File type. Either yara or sigma.')
 	parser.add_argument('-c', '--code', help='Code to search in github repositories.')
 
 	args = parser.parse_args()
 
 	if args.file_type:
-		search_osint_files(file_type=args.file_type)
+		if args.code:
+			search_osint_files(file_type=args.file_type, code=args.code)
+		else:
+			search_osint_files(file_type=args.file_type)
 	elif args.code:
-		search_osint_files(code=args.code)
+		if args.code:
+			search_osint_files(file_type=args.file_type, code=args.code)
+		else:
+			search_osint_files(code=args.code)
--- osint.py
+++ osint.py
@@ -11,12 +11,18 @@ tkn = creds.GITHUB_API
 g = Github(tkn)
 
 
-def search_osint_files(file_type='yara', code=None):
+def search_osint_files(file_type=None, code=None):
 	if file_type == 'yara':
-		query = f'in:file language:'+file_type
+		if code is None:
+			query = f'in:file language:'+file_type
+		else:
+			query = f'{code} in:file language:' + file_type
 	elif file_type == 'sigma':
-		query = f'title AND logsource AND detection AND condition in:file extension:yml'
-	elif code is not None:
+		if code is None:
+			query = f'title AND logsource AND detection AND condition AND {code} in:file extension:yml'
+		else:
+			query = f'title AND logsource AND detection AND condition in:file extension:yml'
+	elif code is not None and file_type is None:
 		query = f'{code} in:file language:'
 	rate_limit = g.get_rate_limit()
 	rate = rate_limit.search
